5389ab97ed8ca287b839d232b0f438049e1d5a48
Ahmad Hamdan <me@ahmadhamdan.com>
2023-09-09 07:56:25 +0400

    Adder logic

--- src/components/Adder.jsx
+++ src/components/Adder.jsx
@@ -1,4 +1,7 @@
-import { Button, Chip, Divider, IconButton, MenuItem, OutlinedInput, Select, Switch, Tooltip, css } from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+import { Controller, useFieldArray, useForm } from "react-hook-form";
+import exactMath from "exact-math";
+import { Alert, Button, Chip, Divider, IconButton, MenuItem, OutlinedInput, Select, Switch, Tooltip, css } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
 
@@ -32,60 +35,130 @@ const inputColCss = css`
 `;
 
 const resultCss = css`
-  font-size: 32px;
+  font-size: 22px;
   font-weight: bold;
   text-align: center;
+  word-wrap: break-word;
 `;
 
-const signOptions = [
-  {
-    value: 1,
-    label: "+",
-  },
-  {
-    value: -1,
-    label: "-",
-  },
-];
+const onlyNumbers = value => {
+  const dicimalPointIndex = value.indexOf(".");
+  return value.replace(/[^0-9]/g, (c, i) => (dicimalPointIndex === i ? c : ""));
+};
+
+const sumRows = rows =>
+  rows.reduce((acc, { sign, value, enabled }) => {
+    if (!enabled || value === "") {
+      return acc;
+    }
+    const numberValue = value.replace(/\.$/, "");
+    const exactMathConfig = { returnString: true, eMinus: Infinity, ePlus: Infinity, maxDecimal: Infinity };
+    switch (sign) {
+      case "+":
+        return exactMath.add(acc, numberValue, exactMathConfig);
+      case "-":
+        return exactMath.sub(acc, numberValue, exactMathConfig);
+    }
+    return acc;
+  }, "0");
+
+const formatter = new Intl.NumberFormat("en-US", { roundingPriority: "morePrecision" });
 
 const Adder = () => {
+  const {
+    control,
+    watch,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+    defaultValues: {
+      rows: [
+        { sign: "+", value: "1", enabled: true },
+        { sign: "+", value: "1", enabled: true },
+      ],
+    },
+  });
+  const { fields, append, remove } = useFieldArray({ name: "rows", control });
+  const [result, setResult] = useState(() => sumRows(getValues().rows));
+
+  const handleAddRowClick = useCallback(() => append({ sign: "+", value: "0", enabled: true }), [append]);
+
+  useEffect(() => {
+    const subscription = watch(({ rows }) => setResult(sumRows(rows)));
+    return () => subscription.unsubscribe();
+  }, [watch, setResult]);
+
+  console.log(errors);
+
   return (
     <form css={rootCss}>
       <div css={rowListCss}>
-        <div css={rowCss}>
-          <div css={signColCss}>
-            <Select fullWidth size="small" defaultValue={1}>
-              {signOptions.map(({ label, value }) => (
-                <MenuItem key={value} value={value}>
-                  {label}
-                </MenuItem>
-              ))}
-            </Select>
+        {fields.length === 0 && (
+          <Alert severity="info">Click on the button below to add a new row</Alert>
+        )}
+        {fields.map(({ id }, index) => (
+          <div key={id} css={rowCss}>
+            <div css={signColCss}>
+              <Controller
+                name={`rows.${index}.sign`}
+                control={control}
+                render={({ field: { ref, ...rest } }) => (
+                  <Select fullWidth size="small" inputRef={ref} {...rest}>
+                    {["+", "-"].map(sign => (
+                      <MenuItem key={sign} value={sign}>
+                        {sign}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                )}
+              />
+            </div>
+            <div css={inputColCss}>
+              <Controller
+                name={`rows.${index}.value`}
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { ref, onChange, ...rest } }) => (
+                  <OutlinedInput
+                    fullWidth
+                    size="small"
+                    error={Boolean(errors.rows?.[index]?.value)}
+                    onChange={e => onChange(onlyNumbers(e.target.value))}
+                    inputRef={ref}
+                    {...rest}
+                  />
+                )}
+              />
+            </div>
+            <div>
+              <Controller
+                name={`rows.${index}.enabled`}
+                control={control}
+                render={({ field: { value, ref, ...rest } }) => (
+                  <Tooltip arrow placement="top" title="Toggle disabled/enabled">
+                    <Switch checked={value} inputRef={ref} {...rest} />
+                  </Tooltip>
+                )}
+              />
+            </div>
+            <div>
+              <Tooltip arrow placement="top" title="Delete" onClick={() => remove(index)}>
+                <IconButton>
+                  <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
           </div>
-          <div css={inputColCss}>
-            <OutlinedInput fullWidth size="small" defaultValue="1" />
-          </div>
-          <div>
-            <Tooltip arrow placement="top" title="Toggle disabled/enabled">
-              <Switch defaultChecked />
-            </Tooltip>
-          </div>
-          <div>
-            <Tooltip arrow placement="top" title="Delete">
-              <IconButton>
-                <DeleteIcon />
-              </IconButton>
-            </Tooltip>
-          </div>
-        </div>
-        <Button fullWidth variant="outlined" startIcon={<AddIcon />}>
+        ))}
+        <Button fullWidth variant="outlined" startIcon={<AddIcon />} onClick={handleAddRowClick}>
           Add Row
         </Button>
       </div>
       <Divider>
         <Chip label="=" />
       </Divider>
-      <div css={resultCss}>2</div>
+      <div css={resultCss}>{formatter.format(result)}</div>
     </form>
   );
 };
